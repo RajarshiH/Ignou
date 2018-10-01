b83235685e4ed3c121b2a425b1500898a4b915db
Rajarshi Haldar <rajarshi.haldar@edifixio.com>
2018-10-01 16:36:32 +0530

    Added Armstrong code

--- armstrong.c
+++ armstrong.c
@@ -1,3 +1,21 @@
-#An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+#include <stdio.h>
 
+int main() {
+   int arms = 153; 
+   int check, rem, sum = 0;
 
+   check = arms;
+
+   while(check != 0) {
+      rem = check % 10;
+      sum = sum + (rem * rem * rem);
+      check = check / 10;
+   }
+
+   if(sum == arms) 
+      printf("%d is an armstrong number.\n", arms);
+   else 
+      printf("%d is not an armstrong number.\n", arms);
+      
+   return 0;
+}
